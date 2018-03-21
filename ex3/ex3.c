31b4646f165de27869d4bfbe34a72c38750ab30d
inxiti <32788205+inxiti@users.noreply.github.com>
2018-03-20 20:20:49 -0400

    added name variable and appropriate print statement

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -3,7 +3,9 @@
 int main() {
 	int age = 10;
 	int height = 72;
+	char *name = "Nammme";
 
+	printf("My name is %s.\n", name); 
 	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
 
