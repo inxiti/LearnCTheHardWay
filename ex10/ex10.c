ba2478c14209a008befb96dd4ccadb3fcf10ca2e
inxiti <32788205+inxiti@users.noreply.github.com>
2018-03-30 20:25:08 -0400

    added exercise 10

---
+++ ex10/ex10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // a list of states for demonstrative purposes
+    char *states[] = {
+        "California",
+        "Oregon",
+        "Washington",
+        "Texas"
+    };
+
+    // iterate and print all arguments passed to program
+    for(int i = 1; i < argc; ++i) {
+        printf("argument %d: %s\n", i, argv[i]);
+    }
+
+    // iterate and print all states in the states array
+    for(int i = 0; i < sizeof(states) / sizeof(char *); ++i) {
+        printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
