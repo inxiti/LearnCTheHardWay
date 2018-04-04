c0e41f6ee0ef33faa967c738048061eb147929e5
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-04 15:30:40 -0400

    added ex15 skeleton, formatted ex14 with googles style guide

--- ex14/ex14.c
+++ ex14/ex14.c
@@ -5,38 +5,34 @@
 int can_print_it(char ch);
 void print_letters(char arg[]);
 
-void print_arguments(int argc, char *argv[])
-{
-    int i = 0;
+// definitions
+void print_arguments(int argc, char *argv[]) {
+  int i = 0;
 
-    for(i = 0; i < argc; i++) {
-        print_letters(argv[i]);
+  for (i = 0; i < argc; i++) {
+    print_letters(argv[i]);
+  }
+}
+
+void print_letters(char arg[]) {
+  int i = 0;
+
+  for (i = 0; arg[i] != '\0'; i++) {
+    char ch = arg[i];
+
+    if (can_print_it(ch)) {
+      printf("'%c' == %d ", ch, ch);
     }
+  }
+
+  printf("\n");
 }
 
-void print_letters(char arg[])
-{
-    int i = 0;
+int can_print_it(char ch) { return isalpha(ch) || isblank(ch); }
 
-    for(i = 0; arg[i] != '\0'; i++) {
-        char ch = arg[i];
+// entry
+int main(int argc, char *argv[]) {
+  print_arguments(argc, argv);
 
-        if(can_print_it(ch)) {
-            printf("'%c' == %d ", ch, ch);
-        }
-    }
-
-    printf("\n");
-}
-
-int can_print_it(char ch)
-{
-    return isalpha(ch) || isblank(ch);
-}
-
-int main(int argc, char *argv[])
-{
-    print_arguments(argc, argv);
-
-    return 0;
+  return 0;
 }
