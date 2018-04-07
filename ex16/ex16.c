e9c0f5e229e76c4af210afb0095f1639211b2239
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-06 23:39:55 -0400

    added exercise 16, formatted in mozilla style

--- ex16/ex16.c
+++ ex16/ex16.c
@@ -1,5 +1,75 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-	return 0;
+struct Person
+{
+  char* name;
+  int age;
+  int height;
+  int weight;
+};
+
+struct Person*
+Person_create(char* name, int age, int height, int weight)
+{
+  struct Person* who = malloc(sizeof(struct Person));
+  assert(who != NULL);
+
+  who->name = strdup(name);
+  who->age = age;
+  who->height = height;
+  who->weight = weight;
+
+  return who;
+}
+
+void
+Person_destroy(struct Person* who)
+{
+  assert(who != NULL);
+
+  free(who->name); // comment out and check with valgrind
+  free(who);
+}
+
+void
+Person_print(struct Person* who)
+{
+  printf("Name: %s\n", who->name);
+  printf("\tAge: %d\n", who->age);
+  printf("\tHeight: %d\n", who->height);
+  printf("\tWeight: %d\n", who->weight);
+}
+
+int
+main()
+{
+  struct Person* joe = Person_create("Joe Alex", 32, 64, 140);
+  struct Person* frank = Person_create("Frank Blank", 20, 72, 180);
+
+  printf("Joe is at memory location: %p\n", (void*)joe);
+  Person_print(joe);
+  printf("\n");
+
+  printf("Frank is at memory location: %p\n", (void*)frank);
+  Person_print(frank);
+  printf("\nAging twenty years.\n\n");
+
+  // make everyone age 20 years and print them again
+  joe->age += 20;
+  joe->height -= 2;
+  joe->weight += 40;
+  Person_print(joe);
+
+  frank->age += 20;
+  frank->weight += 20;
+  Person_print(frank);
+
+  // you must destroy all that you create via malloc
+  Person_destroy(joe);
+  Person_destroy(frank);
+
+  return 0;
 }
--- playground.c
+++ playground.c
@@ -19,48 +19,56 @@
 #include <stdio.h>
 
 // functions
-void func(int (*printer)(char *), char *str) {
-    // invoke the pointed to function with the appropriate argument
-    if ((*printer)(str) == 0) {
-        printf("Message printed successfully.\n\n");
-    } else {
-        printf("Message printed unsuccessfully.\n\n");
-    }
+void
+func(int (*printer)(char*), char* str)
+{
+  // invoke the pointed to function with the appropriate argument
+  if ((*printer)(str) == 0) {
+    printf("Message printed successfully.\n\n");
+  } else {
+    printf("Message printed unsuccessfully.\n\n");
+  }
 }
 
-int print_message(char *str) {
-    printf("Printing message: %s\n", str);
+int
+print_message(char* str)
+{
+  printf("Printing message: %s\n", str);
 
-    return 0;
+  return 0;
 }
 
-void func2(int *param) {
-    printf("param's pointed to address is: %p\n", (void *)param);
+void
+func2(int* param)
+{
+  printf("param's pointed to address is: %p\n", (void*)param);
 
-    // *param actually is the value of x, changing it also changes x, it's the
-    // places in memory.
-    *param = 22222;
+  // *param actually is the value of x, changing it also changes x, it's the
+  // places in memory.
+  *param = 22222;
 }
 
 // main
-int main() {
-    char *str = "Hello, world.";
+int
+main()
+{
+  char* str = "Hello, world.";
 
-    // pass a function and a string to func
-    func(&print_message, str);
+  // pass a function and a string to func
+  func(&print_message, str);
 
-    int x = 11111;
-    int *ptr = &x;
+  int x = 11111;
+  int* ptr = &x;
 
-    // showing addresses
-    printf("ptr addy is  : %p\n", (void *)&ptr);    // pass address of ptr itself
-    printf("ptr points to: %p\n", (void *)ptr);
-    printf("x address is : %p\n", (void *)&x);
+  // showing addresses
+  printf("ptr addy is  : %p\n", (void*)&ptr); // pass address of ptr itself
+  printf("ptr points to: %p\n", (void*)ptr);
+  printf("x address is : %p\n", (void*)&x);
 
-    // before and after values, demonstrating the memory is the same
-    printf("\nx before: %d\n", *ptr);
-    func2(ptr);
-    printf("x after : %d\n", *ptr);
+  // before and after values, demonstrating the memory is the same
+  printf("\nx before: %d\n", *ptr);
+  func2(ptr);
+  printf("x after : %d\n", *ptr);
 
-    return 0;
+  return 0;
 }
