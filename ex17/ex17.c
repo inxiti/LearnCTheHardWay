d819d0055fc62c74d7732893572cf17eec6de646
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-06 23:47:50 -0400

    more stubbing

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
 
 int main(int argc, char *argv[]) {
 
