356b2d62b56d9eb912d16f263944c741f1e36e06
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-08 20:51:20 -0400

    filled out ex18, completed

--- ex18/ex18.c
+++ ex18/ex18.c
@@ -22,7 +22,26 @@ typedef int (*compare_cb)(int a, int b);
  */
 int *bubble_sort(int *numbers, int count, compare_cb cmp)
 {
+    int temp = 0;
+    int i = 0;
+    int j = 0;
+    int *target = malloc(count * sizeof(int));
 
+    if (!target) die("Memory error.");
+
+    memcpy(target, numbers, count * sizeof(int));
+
+    for (i = 0; i < count; ++i) {
+        for (j = 0; j < count - 1; ++j) {
+            if (cmp(target[j], target[j+1]) > 0) {
+                temp = target[j+1];
+                target[j+1] = target[j];
+                target[j] = temp;
+            }
+        }
+    }
+
+    return target;
 }
 
 int sorted_order(int a, int b)
@@ -50,11 +69,38 @@ int strange_order(int a, int b)
  */
 void test_sorting(int *numbers, int count, compare_cb cmp)
 {
-    
+    int i = 0;
+    int *sorted = bubble_sort(numbers, count, cmp);
+
+    if (!sorted) die("Failed to sort as requested.");
+
+    for(i = 0; i < count; ++i) {
+        printf("%d ", sorted[i]);
+    }
+    printf("\n");
+
+    free(sorted);
 }
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) die("USAGE: ex18 list_of_numbers");
 
+    int count = argc - 1;
+    int i = 0;
+    char **inputs = argv + 1;
+
+    int *numbers = malloc(count * sizeof(int));
+    if (!numbers) die("Memoory error.");
+
+    for(i = 0; i < count; ++i) {
+        numbers[i] = atoi(inputs[i]);
+    }
+
+    test_sorting(numbers, count, sorted_order);
+    test_sorting(numbers, count, reverse_order);
+    test_sorting(numbers, count, strange_order);
+
+    free(numbers);
 
 	return 0;
 }
