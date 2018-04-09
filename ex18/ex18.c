f290bb9d613f2a0ed3a1b143a911015ccc70f100
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-08 20:56:56 -0400

    stylized in mozillas style

--- ex18/ex18.c
+++ ex18/ex18.c
@@ -1,17 +1,18 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
 
 /** our old friend die */
-void die(const char *message)
+void
+die(const char* message)
 {
-    if (errno) {
-        perror(message);
-    } else {
-        printf("ERROR: %s\n", message);
-    }
-    exit(1);
+  if (errno) {
+    perror(message);
+  } else {
+    printf("ERROR: %s\n", message);
+  }
+  exit(1);
 }
 
 // typedef creates a fake type, in this case for a function pointer
@@ -20,87 +21,98 @@ typedef int (*compare_cb)(int a, int b);
 /**
     a classic bubble sort function that uses the compare_cb to do the sorting
  */
-int *bubble_sort(int *numbers, int count, compare_cb cmp)
+int*
+bubble_sort(int* numbers, int count, compare_cb cmp)
 {
-    int temp = 0;
-    int i = 0;
-    int j = 0;
-    int *target = malloc(count * sizeof(int));
+  int temp = 0;
+  int i = 0;
+  int j = 0;
+  int* target = malloc(count * sizeof(int));
 
-    if (!target) die("Memory error.");
+  if (!target)
+    die("Memory error.");
 
-    memcpy(target, numbers, count * sizeof(int));
+  memcpy(target, numbers, count * sizeof(int));
 
-    for (i = 0; i < count; ++i) {
-        for (j = 0; j < count - 1; ++j) {
-            if (cmp(target[j], target[j+1]) > 0) {
-                temp = target[j+1];
-                target[j+1] = target[j];
-                target[j] = temp;
-            }
-        }
+  for (i = 0; i < count; ++i) {
+    for (j = 0; j < count - 1; ++j) {
+      if (cmp(target[j], target[j + 1]) > 0) {
+        temp = target[j + 1];
+        target[j + 1] = target[j];
+        target[j] = temp;
+      }
     }
+  }
 
-    return target;
+  return target;
 }
 
-int sorted_order(int a, int b)
+int
+sorted_order(int a, int b)
 {
-    return a - b;
+  return a - b;
 }
 
-int reverse_order(int a, int b)
+int
+reverse_order(int a, int b)
 {
-    return b - a;
+  return b - a;
 }
 
-int strange_order(int a, int b)
+int
+strange_order(int a, int b)
 {
-    if (a == 0 || b == 0) {
-        return 0;
-    }  else {
-        return a % b;
-    }
+  if (a == 0 || b == 0) {
+    return 0;
+  } else {
+    return a % b;
+  }
 }
 
 /**
     used to test that we are sorting things correctly
     by doing the sort, and printing it out
  */
-void test_sorting(int *numbers, int count, compare_cb cmp)
+void
+test_sorting(int* numbers, int count, compare_cb cmp)
 {
-    int i = 0;
-    int *sorted = bubble_sort(numbers, count, cmp);
+  int i = 0;
+  int* sorted = bubble_sort(numbers, count, cmp);
 
-    if (!sorted) die("Failed to sort as requested.");
+  if (!sorted)
+    die("Failed to sort as requested.");
 
-    for(i = 0; i < count; ++i) {
-        printf("%d ", sorted[i]);
-    }
-    printf("\n");
+  for (i = 0; i < count; ++i) {
+    printf("%d ", sorted[i]);
+  }
+  printf("\n");
 
-    free(sorted);
+  free(sorted);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) die("USAGE: ex18 list_of_numbers");
+int
+main(int argc, char* argv[])
+{
+  if (argc < 2)
+    die("USAGE: ex18 list_of_numbers");
 
-    int count = argc - 1;
-    int i = 0;
-    char **inputs = argv + 1;
+  int count = argc - 1;
+  int i = 0;
+  char** inputs = argv + 1;
 
-    int *numbers = malloc(count * sizeof(int));
-    if (!numbers) die("Memoory error.");
+  int* numbers = malloc(count * sizeof(int));
+  if (!numbers)
+    die("Memoory error.");
 
-    for(i = 0; i < count; ++i) {
-        numbers[i] = atoi(inputs[i]);
-    }
+  for (i = 0; i < count; ++i) {
+    numbers[i] = atoi(inputs[i]);
+  }
 
-    test_sorting(numbers, count, sorted_order);
-    test_sorting(numbers, count, reverse_order);
-    test_sorting(numbers, count, strange_order);
+  test_sorting(numbers, count, sorted_order);
+  test_sorting(numbers, count, reverse_order);
+  test_sorting(numbers, count, strange_order);
 
-    free(numbers);
+  free(numbers);
 
-	return 0;
+  return 0;
 }
