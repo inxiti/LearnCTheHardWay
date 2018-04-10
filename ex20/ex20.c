675d67171a64abd84a42042ebf9c74e92ba64bd5
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-10 15:17:06 -0400

    removed unnecessary parenthesis

--- ex20/ex20.c
+++ ex20/ex20.c
@@ -120,12 +120,12 @@ main(int argc, char* argv[])
   test_log_warn();
   test_log_info();
 
-  check((test_check("ex20.c") == 0), "failed with ex20.c");
-  check((test_check(argv[1]) == -1), "failed with argv.");
-  check((test_sentinel(1) == 0), "test_sentinel failed.");
-  check((test_sentinel(100) == -1), "test_sentinel failed.");
-  check((test_check_mem() == -1), "test_check_mem failed.");
-  check((test_check_debug() == -1), "test_check_debug failed.");
+  check(test_check("ex20.c") == 0, "failed with ex20.c");
+  check(test_check(argv[1]) == -1, "failed with argv.");
+  check(test_sentinel(1) == 0, "test_sentinel failed.");
+  check(test_sentinel(100) == -1, "test_sentinel failed.");
+  check(test_check_mem() == -1, "test_check_mem failed.");
+  check(test_check_debug() == -1, "test_check_debug failed.");
 
   return 0;
 
