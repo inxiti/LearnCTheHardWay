2a7bf29e5c14ce5d687dc5a7af33bb4d139f6c8b
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-10 14:48:29 -0400

    stubbed out ex20

--- ex20/ex20.c
+++ ex20/ex20.c
@@ -1,4 +1,46 @@
+#include "dbg.h"
+#include <stdlib.h>
 #include <stdio.h>
+
+void test_debug()
+{
+
+}
+
+void test_log_err()
+{
+
+}
+
+void test_log_warn()
+{
+
+}
+
+void test_log_info()
+{
+
+}
+
+int test_check(cahr *file_name)
+{
+
+}
+
+int test_sentinel(int code)
+{
+
+}
+
+int test_check_mem()
+{
+
+}
+
+int test_check_debug()
+{
+
+}
 
 int main(int argc, char *argv[]) {
 
