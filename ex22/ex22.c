d8111f186b82ebbf08b6ba76bba7b34d70c821ee
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-11 00:21:02 -0400

    stubs

--- ex22/ex22.c
+++ ex22/ex22.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include "ex22.h"
 #include "dbg.h"
 
-int main(int argc, char *argv[]) {
+int THE_SIZE = 1000;
 
+static int THE_AGE = 37;
 
-	return 0;
+int get_age()
+{
+
+}
+
+void set_age(int age) 
+{
+
+}
+
+double update_ratio(double new_ratio)
+{
+
+}
+
+void print_size()
+{
+
 }
---
+++ ex22/ex22_main.c
@@ -0,0 +1,14 @@
+#include "ex22.h"
+#include "dbg.h"
+
+const char *MY_NAME = "Zed A. Shaw";
+
+void scope_demo(int count)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
