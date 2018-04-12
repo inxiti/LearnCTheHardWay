8d97b0973cd92fcefe28e10d1aa8979052c8f880
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-12 11:46:09 -0400

    filled out stubs

--- ex22/ex22.c
+++ ex22/ex22.c
@@ -8,20 +8,25 @@ static int THE_AGE = 37;
 
 int get_age()
 {
-
+    return THE_AGE;
 }
 
 void set_age(int age) 
 {
-
+    THE_AGE = age;
 }
 
 double update_ratio(double new_ratio)
 {
+    static double ratio = 1.0;
 
+    double old_ratio = ratio;
+    ratio = new_ratio;
+
+    return old_ratio;
 }
 
 void print_size()
 {
-
+    log_info("I think size is: %d", THE_SIZE);
 }
