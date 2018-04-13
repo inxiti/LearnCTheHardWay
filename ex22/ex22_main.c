0a02b6733ad081e741576287e963dad162332b51
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-13 13:07:17 -0400

    testing vs code git integration

--- ex22/ex22_main.c
+++ ex22/ex22_main.c
@@ -5,10 +5,24 @@ const char *MY_NAME = "Zed A. Shaw";
 
 void scope_demo(int count)
 {
+    log_info("count is: %d", count);
 
+    if (count > 10) {
+        int count = 100; // BAD! BUGS!
+
+        log_info("count in this scope is: %d", count);
+    }
+
+    log_info("count is at exit: %d", count);
+
+    count = 3000;
+
+    log_info("count after assign: %d", count);
 }
 
 int main(int argc, char *argv[])
 {
+    // test out THE_AGE accessors
+
     return 0;
 }
