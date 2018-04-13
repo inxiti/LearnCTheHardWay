d068f6f1a79bcfc84434a48d9b6adaa6f4e29b31
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-13 18:30:47 -0400

    additional comments showing how code works

--- ex22/ex22.c
+++ ex22/ex22.c
@@ -2,8 +2,12 @@
 #include "ex22.h"
 #include "dbg.h"
 
+// defines the value of THE_SIZE to be 1000, this is it's initial value,
+// thus when accessed in ex22_main.c it is 1000 despite not being defined before
+// being used there in that file.
 int THE_SIZE = 1000;
 
+// global static means file visibility only
 static int THE_AGE = 37;
 
 int get_age()
@@ -18,6 +22,8 @@ void set_age(int age)
 
 double update_ratio(double new_ratio)
 {
+    // function scope static means the value is saved even after the function
+    // completes it's invocation
     static double ratio = 1.0;
 
     double old_ratio = ratio;
