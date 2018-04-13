e46ed60c632adc14b3c8b5fcc95d08b46f29aa64
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-13 17:49:13 -0400

    updated main declaration, and THE_SIZE value

--- ex22/ex22_main.c
+++ ex22/ex22_main.c
@@ -20,9 +20,35 @@ void scope_demo(int count)
     log_info("count after assign: %d", count);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     // test out THE_AGE accessors
+    log_info("my name: %s, age: %d", MY_NAME, get_age());
+
+    set_age(100);
+
+    log_info("my age is now: %d", get_age());
+
+    // test out THE_SIZE extern
+    log_info("THE_SIZE is: %d", THE_SIZE);
+    print_size();
+
+    THE_SIZE = 9;
+
+    log_info("THE_SIZE is now: %d", THE_SIZE);
+    print_size();
+
+    // test the ratio function static
+    log_info("ratio at first: %f", update_ratio(2.0));
+    log_info("ratio again: %f", update_ratio(10.0));
+    log_info("ratio once more: %f", update_ratio(300.0));
+
+    // test the scope demo
+    int count = 4;
+    scope_demo(count);
+    scope_demo(count * 20);
+
+    log_info("count after calling scope_demo: %d", count);
 
     return 0;
 }
