0d057f8452dea2f520cac7af5083c541bc201e4e
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-17 11:23:23 -0400

    stubbing

--- ex24/ex24.c
+++ ex24/ex24.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include "dbg.h"
 
+#define MAX_DATA 100
+
+typedef enum EyeColor {
+	BLUE_EYES, GREEN_EYES, BROWN_EYES,
+	BLACK_EYES, OTHER_EYES
+} EyeColor;
+
+const char *EYE_COLOR_NAMES[] = {
+	"Blue", "Green", "Brown", "Black", "Other"
+};
+
+typedef struct Person {
+	int age;
+	char first_name[MAX_DATA];
+	char last_name[MAX_DATA];
+	EyeColor eyes;
+	float income;
+} Person;
+
 int main(int argc, char *argv[]) {
 
 
