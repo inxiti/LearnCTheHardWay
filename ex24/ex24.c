1e483a2086ccb9e7992a82e099550e47f122dee9
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-24 00:46:40 -0400

    performed some of the extra credit, replaced fscanf's with fgets

--- ex24/ex24.c
+++ ex24/ex24.c
@@ -1,6 +1,7 @@
 // Extra credit: remove uses of fscanf, use functions similar to atoi
 // to convert from string to integer, etc
 #include <stdio.h>
+#include <stdlib.h>
 #include "dbg.h"
 
 #define MAX_DATA 100
@@ -26,6 +27,7 @@ int main() {
 	Person you = {.age = 0};
 	int i = 0;
 	char *in = NULL;
+	char buffer[MAX_DATA];
 
 	printf("What's your first name? ");
 	in = fgets(you.first_name, MAX_DATA-1, stdin);
@@ -36,8 +38,8 @@ int main() {
 	check(in != NULL, "Failed to read last name.");
 
 	printf("How old are you? ");
-	int rc = fscanf(stdin, "%d", &you.age);
-	check(rc > 0, "You have to enter a number.");
+	you.age = atoi(fgets(buffer, MAX_DATA-1, stdin));
+	check(you.age > 0, "You have to enter a number.");
 
 	printf("What color are your eyes:\n");
 	for(i = 0; i <= OTHER_EYES; i++) {
@@ -45,23 +47,23 @@ int main() {
 	}
 	printf("> ");
 
-	int eyes = -1;
-	rc = fscanf(stdin, "%d", &eyes);
-	check(rc > 0, "You have to enter a number.");
+	int eyes = atoi(fgets(buffer, MAX_DATA-1, stdin));
+	check(eyes > 0, "You have to enter a number.");
 	you.eyes = eyes - 1;
 	check(you.eyes <= OTHER_EYES && you.eyes >= 0, "Do it right, that's not an option.");
 
-	printf("How much do you make an hour? ");
-	rc = fscanf(stdin, "%f", &you.income);
-	check(rc > 0, "Enter a floating point number.");
+	printf("How much do you make an hour(in USD)? ");
+	float income = -1.0;
+	income = atof(fgets(buffer, MAX_DATA-1, stdin));
+	you.income = income;
+	check(you.income > 0, "Enter a floating point number.");
 
 	printf("----- RESULTS -----\n");
-
 	printf("First Name: %s", you.first_name);
 	printf("Last Name: %s", you.last_name);
 	printf("Age: %d\n", you.age);
 	printf("Eyes: %s\n", EYE_COLOR_NAMES[you.eyes]);
-	printf("Income: %f\n", you.income);
+	printf("Income: $%.2f/hour\n", you.income);
 
 	return 0;
 
