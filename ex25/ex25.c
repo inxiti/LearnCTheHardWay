d2d74dec84b77c717adccb93398e094659f7f231
inxiti <32788205+inxiti@users.noreply.github.com>
2018-04-23 23:50:45 -0400

    stubbing

--- ex25/ex25.c
+++ ex25/ex25.c
@@ -1,5 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "dbg.h"
+
+#define MAX_DATA 100
+
+int read_string(char **out_string, int max_buffer)
+{
+}
+
+int read_int(int out_int)
+{
+}
 
 int main(int argc, char *argv[]) {
 
