65f0fd228ea3469afef38efc5c1fe598381a4005
inxiti <32788205+inxiti@users.noreply.github.com>
2018-05-01 22:24:56 -0400

    updated script

--- ex26/script.sh
+++ ex26/script.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+BASE_DIRECTORY=~/code/c/ex26
+TMP_DIRECTORY=~/code/c/ex26/tmp
+
 set -e
 
-# go somewhere safe
-cd ~/code/c/ex26/tmp
+# make, and go somewhere safe
+mkdir -p $TMP_DIRECTORY
+cd $TMP_DIRECTORY
 
 # get the source to base APR 1.4.6
 curl -L -O http://archive.apache.org/dist/apr/apr-1.4.6.tar.gz
 
-# extract it and go into the source
+# extract it, and go into the source
 tar -xzvf apr-1.4.6.tar.gz
 cd apr-1.4.6
 
@@ -16,25 +21,23 @@ make
 sudo make install
 
 # reset and clean up
-cd ~/code/c/ex26/tmp
+cd $TMP_DIRECTORY
 rm -rf apr-1.4.6 apr-1.4.6.tar.gz
 
 # do the same with apr-util
 curl -L -O http://archive.apache.org/dist/apr/apr-util-1.4.1.tar.gz
 
-# extract
+# extract, and go into the source
 tar -xzvf apr-util.1.4.1.tar.gz
 cd apr-util-1.4.1
 
 # configure, make, make install
 ./configure --with-apr=/usr/local/apr
-
-# you need that extra parameter to configure because
-# apr-util can't really find it because... who knows <- zed's words
-
 make
 sudo make install
 
 # cleanup
-cd ~/code/c/ex26/tmp
+cd $TMP_DIRECTORY
 rm -rf apr-util-1.4.1* apr-1.4.6*
+cd $BASE_DIRECTORY
+rm -rf $TMP_DIRECTORY
