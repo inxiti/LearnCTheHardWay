d917e4d648c54c658cadaf6a39e0c5055c8ff94b
inxiti <32788205+inxiti@users.noreply.github.com>
2018-05-01 22:07:18 -0400

    finished script

--- ex26/script.sh
+++ ex26/script.sh
@@ -20,3 +20,21 @@ cd ~/code/c/ex26/tmp
 rm -rf apr-1.4.6 apr-1.4.6.tar.gz
 
 # do the same with apr-util
+curl -L -O http://archive.apache.org/dist/apr/apr-util-1.4.1.tar.gz
+
+# extract
+tar -xzvf apr-util.1.4.1.tar.gz
+cd apr-util-1.4.1
+
+# configure, make, make install
+./configure --with-apr=/usr/local/apr
+
+# you need that extra parameter to configure because
+# apr-util can't really find it because... who knows <- zed's words
+
+make
+sudo make install
+
+# cleanup
+cd ~/code/c/ex26/tmp
+rm -rf apr-util-1.4.1* apr-1.4.6*
