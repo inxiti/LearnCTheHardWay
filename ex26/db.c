60a1c78532c6e562ad5e59f27f902a2059de94eb
inxiti <32788205+inxiti@users.noreply.github.com>
2018-06-10 00:02:01 -0400

    stubbed out

--- ex26/db.c
+++ ex26/db.c
@@ -4,4 +4,39 @@
 
 #include "db.h"
 #include "bstrlib.h"
-#include "apache2/modperl_debug.h"
+#include "dbg.h"
+
+static FILE *DB_open(const char *path, const char *mode)
+{
+
+}
+
+static void DB_close(FILE *db)
+{
+
+}
+
+static bstring DB_load()
+{
+    
+}
+
+int DB_update(const char *url)
+{
+
+}
+
+int DB_find(const char *url)
+{
+
+}
+
+int DB_init()
+{
+
+}
+
+int DB_list()
+{
+
+}
