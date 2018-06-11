58afefd3a5b68d4ccc69de2dae069a2fde512baa
inxiti <32788205+inxiti@users.noreply.github.com>
2018-06-11 17:24:33 -0400

    defining more functions

--- ex26/db.c
+++ ex26/db.c
@@ -8,17 +8,33 @@
 
 static FILE *DB_open(const char *path, const char *mode)
 {
-
+    return fopen(path, mode);
 }
 
 static void DB_close(FILE *db)
 {
-
+    fclose(db);
 }
 
 static bstring DB_load()
 {
+    FILE *db = NULL;
+    bstring data = NULL;
+
+    db = DB_open(DB_FILE, "r");
+    check(db, "Failed to open database: %s", DB_FILE);
+
+    data = bread((bNread)fread, db);
+    check(data, "Failed to read from db file: %s", DB_FILE);
     
+    DB_close(db);
+    
+    return data;
+
+error:
+    if (db) DB_close(db);
+    if (data) bdestroy(data);
+    return NULL;
 }
 
 int DB_update(const char *url)
