f76c8de5c2fc15fbce1450a03144ee5a35f6b874
inxiti <32788205+inxiti@users.noreply.github.com>
2018-06-15 00:13:34 -0400

    completing the file, adding structs

--- ex26/shell.c
+++ ex26/shell.c
@@ -11,3 +11,45 @@ int Shell_run(apr_pool_t *p, Shell *cmd)
 {
 
 }
+
+Shell CLEANUP_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell GIT_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell TAR_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell CURL_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell CONFIGURE_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell MAKE_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
+
+Shell INSTALL_SH = {
+    .exe = "",
+    .dir = "",
+    .args = {"", "", "", NULL}
+};
