03fb065463b1150d4fd291c54a3b051aa9092ff6
inxiti <32788205+inxiti@users.noreply.github.com>
2018-06-21 01:01:34 -0400

    stubbed devpkg and main()

---
+++ ex26/devpkg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <apr_general.h>
+#include <apr_getopt.h>
+#include <apr_strings.h>
+#include <apr_lib.h>
+
+#include "dbg.h"
+#include "db.h"
+#include "commands.h"
+
+int main(int argc, const char const *argv[])
+{
+    return 0;
+}
