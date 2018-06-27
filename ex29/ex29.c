b3ce27bafad271f1ef278e2c7d00b47142bd71cf
inxiti <32788205+inxiti@users.noreply.github.com>
2018-06-27 00:08:42 -0400

    stubbed ex29, completed libex29

--- ex29/ex29.c
+++ ex29/ex29.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <dlfcn.h>
 #include "dbg.h"
+
+typedef int (*lib_function)(const char *data);
 
 int main(int argc, char *argv[])
 {
+	int rc = 0;
+	check(argc == 4, "USAGE: ex20 libex29.so function data");
 
 	return 0;
+
+error:
+	return 1;
 }
